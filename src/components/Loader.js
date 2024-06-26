e367f36217df399f775623b7ca9f3399117ffac2
hollynight127626 <hollynight127626@gmail.com>
2024-06-26 07:22:04 +0200

    change loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,8 +4,10 @@ import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import '../assets/css/mystyle.css'; // Import your CSS file for styling
 
 const Loader = () => (
-  <div className="loader-container">
-    <FontAwesomeIcon icon={faSpinner} spin size="3x" />
+  <div className='loader-container'>
+    <video autoPlay loop muted className='loader-video'>
+      <source src='../assets/loader.mp4' type='video/mp4' />
+    </video>
   </div>
 );
 
